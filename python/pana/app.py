1bb414bf66e065e10485e88274b3bbe831441422
jessada <jessada@kth>
2014-01-08 16:00:17 +0100

    done a script to filter haplotype and done generating filtered haplotype

--- python/pana/app.py
+++ python/pana/app.py
@@ -5,8 +5,8 @@ from pana.hapassoc.records import HapAssocResRec
 
 def filter_assoc_hap(file_name):
     assoc_hap_file = open(file_name)
-    print assoc_hap_file.readline().strip()
+    sys.stdout.write(assoc_hap_file.readline())
     for line in assoc_hap_file:
         assoc_hap_rec = HapAssocResRec(line.strip().split())
         if assoc_hap_rec.valid_window:
-            print line.strip()
+            sys.stdout.write(line)
